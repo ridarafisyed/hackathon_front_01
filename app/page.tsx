1d0426eff223fccfdf677e7a60919c04be7d5d0d
AURTeck <112095879+aurteck-codehub@users.noreply.github.com>
2023-08-25 15:20:51 +0500

    update the main page

--- app/page.tsx
+++ app/page.tsx
@@ -3,23 +3,23 @@ import HomePage from "@/views/HomePage"
 
 import { sql } from "@vercel/postgres";
 
-export  async function Cart({
-  params
-} : {
-  params: { user: string }
-}): Promise<JSX.Element> {
-  const { rows } = await sql`SELECT * from CARTS where user_id=${params.user}`;
+// export  async function Cart({
+//   params
+// } : {
+//   params: { user: string }
+// }): Promise<JSX.Element> {
+//   const { rows } = await sql`SELECT * from CARTS where user_id=${params.user}`;
 
-  return (
-    <div>
-      {rows.map((row) => (
-        <div key={row.id}>
-          {row.id} - {row.quantity}
-        </div>
-      ))}
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       {rows.map((row) => (
+//         <div key={row.id}>
+//           {row.id} - {row.quantity}
+//         </div>
+//       ))}
+//     </div>
+//   );
+// }
 
 export default async function IndexPage() {
   const products = await getProducts()
