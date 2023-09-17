7a252ba32364f35ab348bcd0fb5869906b8cceac
AURTeck <112095879+aurteck-codehub@users.noreply.github.com>
2023-09-17 23:21:45 +0500

    update the stripe

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,9 +1,38 @@
-import React from 'react'
+"use client";
+import getStipePromise from "@/lib/stripe";
+import { RootState } from "@/store/store"
 
-const page = () => {
+import { useSelector, useDispatch } from "react-redux"
+
+
+
+const StripeCheckOutButton = () => {
+  const products = useSelector((state:RootState) => state.cart.items);
+  const handleCheckout = async () => {
+    const stripe = await getStipePromise();
+    const response = await fetch("/api/stripe/", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      cache: "no-cache",
+      body: JSON.stringify(products),
+    });
+
+    const data = await response.json();
+    if (data.session) {
+      stripe?.redirectToCheckout({ sessionId: data.session.id });
+    }
+  };
+
   return (
-    <div>Checkout page here!</div>
-  )
-}
+    <div className="py-5">
+      <button
+        className="bg-green-500 py-3 px-3 rounded-md"
+        onClick={handleCheckout}
+      >
+        Check out
+      </button>
+    </div>
+  );
+};
 
-export default page
+export default StripeCheckOutButton;
---
+++ app/success/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Success = () => {
+  return (
+    <div>Checkout is Successfull</div>
+  )
+}
+
+export default Success
